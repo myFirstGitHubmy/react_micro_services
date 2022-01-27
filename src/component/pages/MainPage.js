0607436be6e461280cbb70159ae89d7b7ca1ee95
evbor <ev.borodin1998@gmail.com>
2022-01-28 01:02:38 +0500

    add drug element

--- src/component/pages/MainPage.js
+++ src/component/pages/MainPage.js
@@ -1,9 +1,40 @@
 import React from "react";
+import {onDragEnd, onDragOver, onDragStart, onDrop} from "./drag/DragEvent";
 
 export default function MainPage() {
-    return (
+    function eventClickToInput(event){
+        event.addEventListener('click',()=> {return <input type="text"/>})
+        event.addEventListener('focusout', ()=> {return <p>124</p>})
+        return <p>124</p>
+    }
+
+        return (
         <div>
-            <p>Main page</p>
+            <div className="example-parent">
+                <div
+                    className="example-origin"
+                    onDragOver={event => onDragOver(event)}
+                    onDrop={event => onDrop(event)}
+                >
+                    <div
+                        id="draggable-1"
+                        className="example-draggable"
+                        draggable="true"
+                        onDragStart={event => onDragStart(event)}
+                        onDragEnd={event => onDragEnd(event)}
+                    >
+                        <p>124</p>
+                    </div>
+                </div>
+
+                <div
+                    className="example-dropzone"
+                    onDragOver={event => onDragOver(event)}
+                    onDrop={event => onDrop(event)}
+                >
+                    dropzone
+                </div>
+            </div>
         </div>
     )
 }
